be0f75fb10bb6406cd911b1cace1ed69dc15c30c
Brian Keegan <brian.morris.keegan@gmail.com>
2022-03-27 08:25:57 -0400

    Up to import step

---
+++ Assets/Driver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] int turnSpeed = 150;
+
+    [SerializeField] int accelarationSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        float accelarationAmount = Input.GetAxis("Vertical") * accelarationSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, accelarationAmount, 0);
+    }
+}
